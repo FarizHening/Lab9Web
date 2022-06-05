cb393956bba9b3c01c0cc8f18379b7298bafcdb8
Fariz Hening <farizhening22@gmail.com>
2022-06-05 19:00:43 +0700

    Menambahkan File PHP

---
+++ lab9_php_modular/about.php
@@ -0,0 +1,8 @@
+<?php require('header.php'); ?>
+<link href="style.css" rel="stylesheet" type="text/css">
+<div class="content">
+    <h2>Ini Halaman About</h2>
+    <p>Ini adalah bagian Content dari halaman</p>
+</div>
+
+<?php require('footer.php'); ?>
---
+++ lab9_php_modular/home.php
@@ -0,0 +1,9 @@
+<?php require('header.php'); ?>
+<link href="style.css" rel="stylesheet" type="text/css">
+
+<div class="content">
+    <h2>Ini Halaman Home</h2>
+    <p>Ini adalah bagian Content dari halaman. </p>
+</div>
+
+<?php require('footer.php'); ?>
